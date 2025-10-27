3be81b12a955593b8b1436a78a614470e39e5098
Irwanda Agus <Irwanff41@gmail.com>
2025-10-27 09:50:55 +0700

    add fitur baru

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== Program Nota Restoran ===");
+        double harga = 20000;
+        double pajak = harga * 0.10;
+        double total = harga + pajak;
+
         System.out.println("Pesanan: Nasi Goreng");
-        System.out.println("Harga: Rp20.000");
-        System.out.println("Total Bayar: Rp20.000");
+        System.out.println("Harga: Rp" + harga);
+        System.out.println("Pajak (10%): Rp" + pajak);
+        System.out.println("Total Bayar: Rp" + total);
     }
 }
